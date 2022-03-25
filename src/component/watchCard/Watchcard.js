d88ba4ed147baecb0818e3cc231c6ecfa47ff4a6
shariarhossain23e <shariarhossain23@gamil.comm>
2022-03-25 21:07:28 +0600

    shol ui all data

--- src/component/watchCard/Watchcard.js
+++ src/component/watchCard/Watchcard.js
@@ -3,12 +3,15 @@ import './watchcart.css';
 
 const Watchcard = ({watch}) => {
     console.log(watch)
-    const {img,name,price} = watch;
+    const {img,name,balance} = watch;
     return (
         <div className='card'>
             <div className='card-img'>
             <img src={img}  alt="" ></img>
             </div>
+            <h2>Name:{name}</h2>
+            <h2> price: ${balance}</h2>
+            <button>Add to cart</button>
         </div>
     );
 };
